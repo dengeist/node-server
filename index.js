980d460ca9a57e72a119929c8b33abdeefe60989
dengeist <eliasjmason@gmail.com>
2016-04-26 12:16:39 -0700

    Changes for local client-server communication.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var express = require("express");
 var app = express();
+var person = {
+    name: 'Joe Schmoe',
+    hobbies: 'Being a schmuck.',
+    profession: 'Government Issued Supersoldier'
+};
 
-
-
-app.get('/headers', function(request, response) {
-    response.send(request.headers);
+app.get('/', function(request, response){
+  response.send(person);
 });
 
-app.get('/headers/:header_name', function(request, response) {
-    var headers = request.headers;
-    var headerName = request.params.header_name;
-    response.send(headers[headerName]);
+app.get('/headers', function(request, response){
+  response.send(request.headers);
+});
+
+app.get('/headers/:header_name', function(request, response){
+  response.send(request.headers[request.params.header_name]);
+  console.log(request.params.header_name);
+
+});
+
+app.get('/version', function(request, response){
+  response.send(request.httpVersion);
+  console.log(request.httpVersion);
+
 });
 
 app.listen(8080);
